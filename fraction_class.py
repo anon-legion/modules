cc1aaa3ceab38c7b3e7e44529dfa0b7bdffc8c5f
=GV= <77139649+anon-legion@users.noreply.github.com>
2021-01-25 14:30:57 +0800

    update fraction_class code to import gcd module

--- fraction_class.py
+++ fraction_class.py
@@ -2,8 +2,9 @@
 """
 Created on Sun Nov  8 19:58:54 2020
 
-@author: Anon
+@author: =GV=
 """
+from GCD_euclids_recursion import gcdRecur as gcd
 
 class fraction(object):
     def __init__(self, numer, denom):
@@ -26,31 +27,18 @@ class fraction(object):
         newNumer = self.numerator() * other.denominator() - other.numerator() * self.denominator()
         return fraction(newNumer, newDenom).lowest()
     def convert(self):
-        return self.numerator() / self.denominator()
-    def gcd(self, a, b):
-        """
-        input:
-            a, b: assumes any positive integer
-        output:
-            given the appropriate inputs function returns the Greatest Common Denominator of a and b respectively
-        
-        NOTE:   is used as a helper function for lowest()       
-        """
-        if b == 0:
-            return a
-        else:
-            return self.gcd(b, a % b)
+        return self.numerator() / self.denominator()    
     def lowest(self):
-        newNumer = int(self.numerator() / self.gcd(self.numerator(), self.denominator()))
-        newDenom = int(self.denominator() / self.gcd(self.numerator(), self.denominator()))
+        newNumer = int(self.numerator() / gcd(self.numerator(), self.denominator()))
+        newDenom = int(self.denominator() / gcd(self.numerator(), self.denominator()))
         return fraction(newNumer, newDenom)
 
 
 # test
-oneThirds = fraction(1, 3)
-threeFourths = fraction(15, 20)
-total = oneThirds + threeFourths
-sub = total - fraction(17,22)
-print(total)
-print(sub)
-print(repr(sub))
+# oneThirds = fraction(1, 3)
+# threeFourths = fraction(15, 20)
+# total = oneThirds + threeFourths
+# sub = total - fraction(17,22)
+# print(total)
+# print(sub)
+# print(repr(sub))
